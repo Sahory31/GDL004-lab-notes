4c8ace9ad9d9d5a249392f8575c53c1d5c430e7a
Karla <karla.alvarez@michelada.io>
2020-03-26 22:20:47 -0600

    start of SPA implementation using react-router

---
+++ src/components/Auth/trash.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Trash extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hola, Esta es tu papelera</h1>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Router, Route, browserHistory } from 'react-router';
+import Home from './components/Auth/signUp-In';
+import MainPage from './components/Auth/mainPage';
+import Trash from './components/Auth/trash';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router history={browserHistory}>
+    <Route path="/home" component={Home}></Route>
+    <Route path="/mainPage" component={MainPage}></Route>
+    <Route path="/trash" component={Trash}></Route>
+  </Router>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
