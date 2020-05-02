475e27d3ffe6c7ba12ab20c950befdde07198759
Karla <karla.alvarez@michelada.io>
2020-05-01 21:39:55 -0500

    Updates

---
+++ precache-manifest.d5a30f4984a25ba702b2fac71f0ceca6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bafac2534d2e4c69c9b3a1b03a0802f",
+    "url": "/GDL004-lab-notes/index.html"
+  },
+  {
+    "revision": "20858973979af2ecbf3d",
+    "url": "/GDL004-lab-notes/static/css/main.3ff51c66.chunk.css"
+  },
+  {
+    "revision": "f815582489415c619eab",
+    "url": "/GDL004-lab-notes/static/js/2.a7e38473.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/GDL004-lab-notes/static/js/2.a7e38473.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20858973979af2ecbf3d",
+    "url": "/GDL004-lab-notes/static/js/main.1d5b3e34.chunk.js"
+  },
+  {
+    "revision": "ed04df47a1062ac252f6",
+    "url": "/GDL004-lab-notes/static/js/runtime-main.80d52ec4.js"
+  },
+  {
+    "revision": "756a1e2bd5022d44b8d5b024d51e820e",
+    "url": "/GDL004-lab-notes/static/media/backgroundStart.756a1e2b.png"
+  }
+]);
